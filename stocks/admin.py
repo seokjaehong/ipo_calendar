ecefc5d82da8f81fad86b69b6f00ac815a0a086a
hong <hsj2334@gmail.com>
2021-07-04 20:51:20 +0900

    first commit

---
+++ stocks/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from stocks.models import IPO
+
+admin.site.register(IPO)
+
---
+++ stocks/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from stocks.crawler import StockCrawler
+from stocks.serializer import IPOSerializer
+
+
+class Run(APIView):
+    def get(self, *args, **kwargs):
+        s = StockCrawler()
+        result = s.get_list()
+        serializer = IPOSerializer(result, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
